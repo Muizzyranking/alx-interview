67c9c5c4ff3d25a69cfb16abb602ec35b3f4e8a6
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-09-24 14:01:20 +0100

    Change comes from within
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module for makeChange function"""
+
+
+def makeChange(coins, total):
+    """
+    Function to determine the fewest number of coins to make up total
+
+    Args:
+        coins: list of the values of the coins in your possession
+    total: total amount to be made up
+
+    Returns:
+        0 if total is 0 or less
+    -1 if total cannot be made up by any
+    fewest number of coins to meet total otherwise
+    """
+    if total <= 0:
+        return 0
+
+    if not coins:
+        return -1
+
+    coins.sort(reverse=True)
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
+
+
+print(makeChange([1, 2, 25], 37))
+print(makeChange([1, 2, 37], 37))
+
+print(makeChange([1256, 54, 48, 16, 102], 1453))
