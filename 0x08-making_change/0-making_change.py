8f0454c8cca5589965c798a3ae1c898f5c5f267d
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-09-24 14:03:59 +0100

    (fix): remove test prints
    Signed-off-by: Muizzyranking <oyebowaleabdulmuiz@gmail.com>

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -30,9 +30,3 @@ def makeChange(coins, total):
             dp[x] = min(dp[x], dp[x - coin] + 1)
 
     return dp[total] if dp[total] != float('inf') else -1
-
-
-print(makeChange([1, 2, 25], 37))
-print(makeChange([1, 2, 37], 37))
-
-print(makeChange([1256, 54, 48, 16, 102], 1453))
